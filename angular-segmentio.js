38d40ea63f1f93de1ab7e0b0957cd9c48a429d0b
Di Wu <dw323@cornell.edu>
2015-07-07 22:33:50 -0700

    remove logging

--- angular-segmentio.js
+++ angular-segmentio.js
@@ -11,12 +11,12 @@ angular.module('segmentio', ['ng'])
             var methodFactory = function(type) {
                 return function() {
                     var args = Array.prototype.slice.call(arguments, 0);
-                    $log.debug('Call segmentio API with', type, args);
+                    // $log.debug('Call segmentio API with', type, args);
                     if ($window.analytics.initialized) {
-                        $log.debug('Segmentio API initialized, calling API');
+                        // $log.debug('Segmentio API initialized, calling API');
                         $window.analytics[type].apply($window.analytics, args);
                     } else {
-                        $log.debug('Segmentio API not yet initialized, queueing call');
+                        // $log.debug('Segmentio API not yet initialized, queueing call');
                         $window.analytics.push([type].concat(args));
                     }
                 };
